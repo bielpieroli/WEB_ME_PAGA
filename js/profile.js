e33286e6b494b3833e12ccd7174bd7ea3838fc90
Pedro <pedrohpdias10@gmail.com>
2025-06-14 23:20:50 -0300

    estilo no perfil

---
+++ js/profile.js
@@ -0,0 +1,3 @@
+document.getElementById('username').textContent = currentUser.name;
+document.getElementById('email').textContent = currentUser.email;
+document.getElementById('profile-pic').src = currentUser.pic;
